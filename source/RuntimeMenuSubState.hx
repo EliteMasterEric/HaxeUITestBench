85ab3f7709480730c59e2bdb24b89e8b79c57395
EliteMasterEric <ericmyllyoja@gmail.com>
2023-09-15 18:10:52 -0400

    Add basic form to RuntimeMenuSubState

--- source/RuntimeMenuSubState.hx
+++ source/RuntimeMenuSubState.hx
@@ -1,21 +1,28 @@
 package;
 
 import flixel.FlxBasic;
+import flixel.FlxCamera;
 import flixel.FlxG;
 import flixel.FlxSprite;
+import flixel.FlxState;
 import flixel.graphics.frames.FlxFrame;
 import haxe.ui.components.Button;
 import haxe.ui.components.CheckBox;
+import haxe.ui.components.TextField;
 import haxe.ui.containers.Form;
 import haxe.ui.containers.ListView;
 import haxe.ui.containers.SideBar;
+import haxe.ui.containers.VBox;
+import haxe.ui.containers.dialogs.Dialog;
 import haxe.ui.containers.menus.MenuCheckBox;
 import haxe.ui.core.Component;
 import haxe.ui.events.UIEvent;
 import haxe.ui.macros.ComponentMacros;
-import openfl.text.TextField;
+import lime.app.Future;
+import openfl.Assets;
+import openfl.display.BitmapData;
 
-class RuntimeMenuState extends HaxeUIState // extends MusicBeatState
+class RuntimeMenuSubState extends HaxeUISubState // extends MusicBeatState
 {
 	public function new()
 	{
@@ -45,6 +52,26 @@ class RuntimeMenuState extends HaxeUIState // extends MusicBeatState
 		{
 			trace('Changed: ${event.value}');
 		}
+
+		var testForm:Form = component.findComponent("testForm", Form);
+		var testFormValue:TextField = component.findComponent("testFormValue", TextField);
+		var testFormSubmit:Button = component.findComponent("testFormSubmit", Button);
+
+		testFormSubmit.onClick = function(event:UIEvent)
+		{
+			trace('Clicked form button');
+			testForm.submit();
+		}
+
+		testForm.onSubmit = function(event:UIEvent)
+		{
+			trace('Form submitted! Value: ${testFormValue.text}');
+		}
+
+		testForm.onInvalidData = function(event:UIEvent)
+		{
+			trace('Invalid data!');
+		}
 	}
 
 	override function update(elapsed:Float)
@@ -69,6 +96,23 @@ class RuntimeMenuState extends HaxeUIState // extends MusicBeatState
 			component = buildComponent(_componentKey);
 			this.add(component);
 		}
+
+		if (FlxG.keys.justPressed.D)
+		{
+			var dialog:Dialog = cast this.buildComponent("assets/data/dialog.xml");
+			dialog.destroyOnClose = false;
+			dialog.closable = true;
+			dialog.showDialog(true);
+		}
+
+		if (FlxG.keys.justPressed.C)
+		{
+			var future:Future<BitmapData> = Assets.loadBitmapData('assets/images/cursor-default.png');
+			future.onComplete(function(bitmapData:BitmapData)
+			{
+				FlxG.mouse.load(bitmapData, 1.0, 0, 0);
+			});
+		}
 	}
 
 	var menuBG:FlxSprite;
@@ -82,5 +126,25 @@ class RuntimeMenuState extends HaxeUIState // extends MusicBeatState
 		menuBG.updateHitbox();
 		menuBG.screenCenter();
 		menuBG.scrollFactor.set(0, 0);
+	}
+}
+
+/**
+ * All this state does is launch the substate.
+ */
+class RuntimeMenuSubStateParent extends FlxState
+{
+	public function new()
+	{
+		super();
+	}
+
+	var secondCamera:FlxCamera;
+
+	public override function create():Void
+	{
+		super.create();
+
+		openSubState(new RuntimeMenuSubState());
 	}
 }
